9f1f9d9638381e5e0c44826e3527504b20280270
mridulpurohit <mridulp100@gmail.com>
2023-08-17 10:18:32 +0530

    Update aws-resource-tracker.sh

--- report-the-aws-resource-usage/aws-resource-tracker.sh
+++ report-the-aws-resource-usage/aws-resource-tracker.sh
@@ -11,17 +11,17 @@ set -x
 # AWS IAM Users
 
 # list s3 buckets
-echo "Print list of s3 buckets"
+echo "Print list of s3 buckets" >> resourceTracker
 aws s3 ls >> resourceTracker
 
 # list EC2 instances
-echo "Print list of ec2 instances"
+echo "Print list of ec2 instances" >> resourceTracker
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> resourceTracker
 
 # list lambda
-echo "Print list of lambda functions"
+echo "Print list of lambda functions" >> resourceTracker
 aws lambda list-functions >> resourceTracker
 
 # list IAM users
-echo "Print list of IAM Users"
+echo "Print list of IAM Users" >> resourceTracker
 aws iam list-users >> resourceTracker
